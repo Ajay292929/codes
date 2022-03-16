fb0a5f6084db76ad6eb4e559ae02db38f5032f86
Ajay292929 <92911204+Ajay292929@users.noreply.github.com>
2022-03-16 12:33:27 +0530

    Create Custominput.js

---
+++ Custominput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CustomInput =  () => {
+  
+    return (
+
+        <View>
+
+      <Text> hi i am back </Text>
+      
+      </View>
+ )
+}
+
+export default CustomInput
